6a1caa76e6e9ff9fe21b5790a76d0f25829e73ca
Raheelsaeed007 <113680419+Raheelsaeed007@users.noreply.github.com>
2023-12-04 13:15:42 +0400

    Delete Chapters/Chapter 1 directory

--- Chapters/Chapter 1/Assessment1Ex5.py
+++
@@ -1,9 +0,0 @@
-count = 0
-while True:
-    user_input = input("Do you want to continue? (Y/N):")
-    if user_input.upper() == 'Y':
-        count += 1
-    else:
-        break
-
-    print(f"The loop was executed{count} times.")
--- Chapters/Chapter 1/Assessment1Ex7.py
+++
@@ -1,4 +0,0 @@
-for a in range(1,101):
-    if a % 2 !=0:
-        continue
-    print(a)
